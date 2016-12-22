bfd358a991bf0bf57296ef36f88631aef958a46c
atiyka <atiyka@gmail.com>
2016-12-22 16:25:34 +0200

    Creating HomeView class

--- js/main.js
+++ js/main.js
@@ -1,17 +1,4 @@
 var app = {
-
-    renderHomeView: function() {
-        $('body').html(this.homeTpl);
-        $('.search-key').on('keyup', $.proxy(this.findByName, this));
-    },
-
-    findByName: function() {
-        console.log('findByName');
-        this.store.findByName($('.search-key').val(), function(employees) {
-            $('.employee-list').html(self.employeeLiTpl(employees));
-        });
-    },
-
     showAlert: function (message, title) {
         if (navigator.notification) {
             navigator.notification.alert(message, null, title, 'OK');
@@ -22,11 +9,8 @@ var app = {
 
     initialize: function() {
         var self = this;
-        this.homeTpl = Handlebars.compile($("#home-tpl").html());
-        this.employeeLiTpl = Handlebars.compile($("#employee-li-tpl").html());
-
         this.store = new MemoryStore(function() {
-            self.renderHomeView();
+            $('body').html(new HomeView(self.store).render().el);
         });
     }
 
