1a6f04c48159ab51f5c76510f78ddd0a23fe114d
atiyka <atiyka@gmail.com>
2016-12-23 14:09:16 +0200

    Check geolocation

--- js/EmployeeView.js
+++ js/EmployeeView.js
@@ -13,17 +13,21 @@ var EmployeeView = function(employee) {
 	this.addLocation = function(event) {
 	    event.preventDefault();
 	    console.log('addLocation');
-	    navigator.geolocation.getCurrentPosition(
-	        function(position) {
-	        	console.log('succeed 1');
-	            $('.location', this.el).html(position.coords.latitude + ', ' + position.coords.longitude);
-	            console.log('succeed 2');
-	        },
-	        function() {
-	        	console.log('fail 1');
-	            app.showAlert('Error getting location', 'Error');
-	            console.log('fail 2');
-	        });
+	    if ("geolocation" in navigator) {
+		    navigator.geolocation.getCurrentPosition(
+		        function(position) {
+		        	console.log('succeed 1');
+		            $('.location', this.el).html(position.coords.latitude + ', ' + position.coords.longitude);
+		            console.log('succeed 2');
+		        },
+		        function() {
+		        	console.log('fail 1');
+		            app.showAlert('Error getting location', 'Error');
+		            console.log('fail 2');
+		        });
+		} else {
+			app.showAlert('Location not supported', 'Error');
+		}
 	    return false;
 	};
 
